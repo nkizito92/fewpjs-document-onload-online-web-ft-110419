612584ff398a00be37b32ae059e938c6a2352ae8
Kizito Njoku <nkizito57@gmail.com>
2020-03-04 03:48:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Your code goes here
 document.addEventListener("DOMContentLoaded", function() {
-  
+  text.innerHTML = "This is really cool!"
 });
 var text = document.querySelector("#text")
 console.log("This is really cool!")
