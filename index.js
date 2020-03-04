107579e74daaa890e42ed5368fd95cbf7e6ffd7c
Kizito Njoku <nkizito57@gmail.com>
2020-03-04 03:48:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,4 +3,3 @@ document.addEventListener("DOMContentLoaded", function() {
   text.innerHTML = "This is really cool!"
 });
 var text = document.querySelector("#text")
-console.log("This is really cool!")
