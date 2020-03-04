c2ad8e551757554761af67dbffda3bef3ba3f441
Kizito Njoku <nkizito57@gmail.com>
2020-03-04 03:46:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 // Your code goes here
 document.addEventListener("DOMContentLoaded", function() {
-  console.log("The DOM has loaded");
+  console.log("JavaScript is so cool. It lets me add text to my page programmatically.");
 });
+
+console.log("This is really cool!")
